8d0ea79e57c31daf341116507922cbe3b9c80adb
haji <muhamedhajajii@outlook.com>
2025-02-28 13:42:00 +0200

    dist

--- api/index.js
+++ api/index.js
@@ -1,14 +1,3 @@
-export default async function handler(req, res) {
-  const { default: app } = await import(
-    "../dist/social-media-app/server/server.mjs"
-  );
-
-  console.log(app); // Log the imported module
-
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    app(req, res, parsedUrl);
-  });
-
-  return server.emit("request", req, res);
-}
+export default import("../dist/social-media-app/server/server.mjs").then(
+  (module) => module.app()
+);
