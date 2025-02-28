7ed047a42775966d173991e595eae355384ba607
haji <muhamedhajajii@outlook.com>
2025-02-28 12:43:23 +0200

    dist

--- api/index.js
+++
@@ -1,8 +0,0 @@
-async function loadServer() {
-  const serverModule = await import(
-    "../dist/social-media-app/server/server.mjs"
-  );
-  return serverModule.app;
-}
-
-export default loadServer().then((app) => app());
