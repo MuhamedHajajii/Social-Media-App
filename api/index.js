f90489c2e7e7da2ee720d45fc071e5d109b48ed6
haji <muhamedhajajii@outlook.com>
2025-02-28 12:33:33 +0200

    dist

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-export default import("../dist/social-media-app/server/server.mjs").then(
-  (module) => module.app()
-);
+const server = require("../dist/social-media-app/server/main");
+
+module.exports = server.app();
